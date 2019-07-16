b6b22a76c8e36153a36587cdc0b05bae194386db
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-16 18:48:33 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,6 @@
 class BankAccount
-  attr_reader :name, :balance, :status
+  attr_reader :name, :status
+  attr_accessor :balance
   def initialize(name)
     @name = name
     @balance = 1000
@@ -11,7 +12,7 @@ class BankAccount
    end 
    
    def display_balance
-    
+    return  "Your balance is $#{1000}."
    end 
    
    def valid?
@@ -19,7 +20,7 @@ class BankAccount
    end
    
    def close_account
-     
+     self.clear
    end 
    
 end
