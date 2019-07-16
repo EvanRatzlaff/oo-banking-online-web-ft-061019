fafc24c08f86e46fa52712a6308444d688ed7c05
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-16 18:12:45 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,25 @@
 class BankAccount
-
+  attr_reader :name, :balance, :status
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    end
+  
+   def deposit(money)
+     @balance += money
+   end 
+   
+   def display_balance
+    
+   end 
+   
+   def valid?
+     
+   end
+   
+   def close_account
+     
+   end 
+   
 end
